dd91b2c115e237e2de15463dae6c5c8db6ebe312
Travis Fowlston <travisfowlston@ymail.com>
2024-02-11 22:22:05 -0800

    finished setting up all the routes for the controllers

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,1 +1,28 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const {
+  getAllThoughts,
+  getThoughtById,
+  createNewThought,
+  updateThought,
+  deleteThought,
+  addNewReaction,
+  deleteReaction,
+} = require("../../controllers/thought-controller");
+
+// Route to retrieve all thoughts and create a new thought
+router.route("/").get(getAllThoughts).post(createNewThought);
+
+// Route to retrieve a single thought, update a thought, and delete a thought by its id
+router
+  .route("/:thoughtId")
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
+
+// Route to add a new reaction to a thought
+router.route("/:thoughtId/reactions").post(addNewReaction);
+
+// Route to delete a reaction from a thought
+router.route("/:thoughtId/reactions/:reactionId").delete(deleteReaction);
+
+module.exports = router;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,1 +1,24 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const {
+  getAllUsers,
+  getUserById,
+  createNewUser,
+  updateUser,
+  deleteUser,
+  addNewFriend,
+  deleteFriend,
+} = require("../../controllers/user-controller");
+
+// Route to retrieve all users and create a new user
+router.route("/").get(getAllUsers).post(createNewUser);
+
+// Route to retrieve a single user, update a user, and delete a user by their id
+router.route("/:userId").get(getUserById).put(updateUser).delete(deleteUser);
+
+// Route to add a new friend and delete a friend from a user's friend list
+router
+  .route("/:userId/friends/:friendId")
+  .post(addNewFriend)
+  .delete(deleteFriend);
+
+module.exports = router;
